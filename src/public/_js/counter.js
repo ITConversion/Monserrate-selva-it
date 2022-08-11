c3e980360196a11b6ae278b694c37835ce33aff8
David Trujillo <dtrujillo@conversionaventa.com>
2022-08-11 08:22:47 -0500

    UPDATE fecha banner

--- src/public/_js/counter.js
+++ src/public/_js/counter.js
@@ -1,5 +1,5 @@
 (function() {
-    var deadline = '2022/08/12 18:00';
+    var deadline = '2022/09/10 18:00';
     function pad(num, size) {
         var s = "0" + num;
         return s.substr(s.length - size);
