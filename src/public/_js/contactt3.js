4f5fa561bbfa1aa2d61351d61bae392d7a905794
ITConversion <109302837+ITConversion@users.noreply.github.com>
2023-07-06 13:06:50 -0600

    form

--- src/public/_js/contactt3.js
+++ src/public/_js/contactt3.js
@@ -38,7 +38,7 @@
       success: function (data) {
         console.log('response from webhook', data)
         $(Messages).html('<p>Enviado exitosamente.</p>')
-        window.location.href = "/agradecimientos/"
+        window.location.href = "/agradecimientos"
       }
     })
   }
